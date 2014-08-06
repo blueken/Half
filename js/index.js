ed613cdc3f1d95fe73edf703f8f5bf39a0b91534
Bob <jiafeifei@doozii.com>
2014-08-06 20:11:19 +0800

    Bob:anchor auto fit ok

--- js/index.js
+++ js/index.js
@@ -7,14 +7,31 @@ $(function() {
 	// 		// autoFitPos('.clever');
 	// 		// autoResizeBg();
 	// }).trigger("resize");
+
+	domAutoFitWindow(".clever");
 	
 });
 
 	
 function bgAutoFitWindow(img_selector) {
-		var theWindow        = $(window),
-	    $bg              = $(img_selector),
-	    aspectRatio      = $bg.width() / $bg.height();
+	/*
+	need 3 styles
+
+	img.img_selector {
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+	img.autofit_window_bgwidth {
+		width: 100%;
+	}
+	img.autofit_window_bgheight {
+		height: 100%;
+	}
+	*/
+	var theWindow        = $(window),
+    $bg              = $(img_selector),
+    aspectRatio      = $bg.width() / $bg.height();
 
 	theWindow.resize(function() {
 		if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
@@ -28,62 +45,83 @@ function bgAutoFitWindow(img_selector) {
 		}
 	}).trigger("resize");
 }
+function domAutoFitWindow(dom_selector) {
+	var theWindow        = $(window);
+	theWindow.resize(function() {
+		autoFitPos(dom_selector);
+	}).trigger("resize");
+}
 
+function autoFitPos(selector, pic_width, pic_height) {
+	//auto set position keeping image width/height ratio
+	/*
+	selector dom need has style set as:
+	selector {
+		position: absolute;
+		left:0;
+		top:0;
+		display: block;
+		text-indent: -2000em;
+	}
+	selector_concret1 {
+		width: 175px;
+		height: 45px;
+		margin-left: 435px;
+		margin-top: 311px;
+	}
+	*/
+	var domObjs = $(selector);
+	var PIC_HEIGHT = pic_width ? pic_width : 960;
+	var PIC_WIDTH = pic_height ? pic_height : 640;
+	var big_fish = 0;
+	var scale = 1;
+	var perfect_ratio = PIC_WIDTH / PIC_HEIGHT;
+	var win_width = $(window).width();
+	var win_height = $(window).height();
 
-// function autoFitPos(selector, pic_width, pic_height) {
-// 	//auto set position keeping image width/height ratio
-// 	var domObjs = $(selector);
-// 	var PIC_HEIGHT = pic_width ? pic_width : 960;
-// 	var PIC_WIDTH = pic_height ? pic_height : 640;
-// 	var big_fish = 0;
-// 	var scale = 1;
-// 	var perfect_ratio = PIC_WIDTH / PIC_HEIGHT;
-// 	var win_width = $(window).width();
-// 	var win_height = $(window).height();
+	if(win_width / win_height < perfect_ratio) {
+		big_fish = win_height;
+		scale = win_height / PIC_HEIGHT;
+	} else {
+		big_fish = win_width;
+		scale = win_width / PIC_WIDTH;
+	}
 
-// 	if(win_width / win_height < perfect_ratio) {
-// 		big_fish = win_height;
-// 		scale = win_height / PIC_HEIGHT;
-// 	} else {
-// 		big_fish = win_width;
-// 		scale = win_width / PIC_WIDTH;
-// 	}
+	// record origin value ,when first autofit
+	if(! autoFitPos.did){
+		autoFitPos.did = [];
+		domObjs.each(function(i, e) {
+			var domObj = $(this);
+			autoFitPos.did[i] = {};
+			autoFitPos.did[i].aml = parseInt(domObj.css("margin-left"));
+			autoFitPos.did[i].amt = parseInt(domObj.css("margin-top"));
+			autoFitPos.did[i].aw = parseInt(domObj.css("width"));
+			autoFitPos.did[i].ah = parseInt(domObj.css("height"));
+		});
+	}
 
-// 	// record origin value ,when first autofit
-// 	if(! autoFitPos.did){
-// 		autoFitPos.did = [];
-// 		domObjs.each(function(i, e) {
-// 			var domObj = $(this);
-// 			autoFitPos.did[i] = {};
-// 			autoFitPos.did[i].aml = parseInt(domObj.css("margin-left"));
-// 			autoFitPos.did[i].amt = parseInt(domObj.css("margin-top"));
-// 			autoFitPos.did[i].aw = parseInt(domObj.css("width"));
-// 			autoFitPos.did[i].ah = parseInt(domObj.css("height"));
-// 		});
-// 	}
-
-// 	domObjs.each(function(i, e) {
-// 		var domObj = $(this);
-// 		var aml = autoFitPos.did[i].aml;
-// 		var amt = autoFitPos.did[i].amt;
-// 		var aw = autoFitPos.did[i].aw;
-// 		var ah = autoFitPos.did[i].ah;
+	domObjs.each(function(i, e) {
+		var domObj = $(this);
+		var aml = autoFitPos.did[i].aml;
+		var amt = autoFitPos.did[i].amt;
+		var aw = autoFitPos.did[i].aw;
+		var ah = autoFitPos.did[i].ah;
 		
-// 		aml *= scale;
-// 		amt *= scale;
-// 		aw *= scale;
-// 		ah *= scale;
+		aml *= scale;
+		amt *= scale;
+		aw *= scale;
+		ah *= scale;
 
-// 		aml += "px";
-// 		amt += "px";
-// 		aw  += "px";
-// 		ah  += "px";
+		aml += "px";
+		amt += "px";
+		aw  += "px";
+		ah  += "px";
 
-// 		domObj.css("margin-left", aml); 
-// 		domObj.css("margin-top", amt);
-// 		domObj.css("width", aw);
-// 		domObj.css("height", ah);
-// 	});
+		domObj.css("margin-left", aml); 
+		domObj.css("margin-top", amt);
+		domObj.css("width", aw);
+		domObj.css("height", ah);
+	});
 
-// 	return scale;
-// }
+	return scale;
+}
